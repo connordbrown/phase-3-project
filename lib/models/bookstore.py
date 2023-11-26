365d0fe204f9db057d94dffdf601f8d006afc3d8
Connor <connordbrown@gmail.com>
2023-11-26 08:52:12 -0700

    add __init__() to bookstore

---
+++ lib/models/bookstore.py
@@ -0,0 +1,12 @@
+from __init__ import CURSOR, CONN
+
+class Bookstore:
+
+    # dictionary of objects saved to the database
+    all = {}
+
+    def __init__(self, name, location, id=None):
+        self.name = name
+        self.location = location
+        self.id = id
+    

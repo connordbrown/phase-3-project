7ba4d77aa9f147d9d36700eeee3498b1ac6465fe
Connor <connordbrown@gmail.com>
2023-11-26 09:00:20 -0700

    add property and setter methods for bookstore

--- lib/models/bookstore.py
+++ lib/models/bookstore.py
@@ -13,3 +13,14 @@ class Bookstore:
     def __repr__(self):
         return f"<Bookstore b{self.id}: {self.name}, {self.location}>"
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise Exception
+        if not len(name):
+            raise Exception
+        self._name = name

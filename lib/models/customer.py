111c8ae85710957f6b759ae6c6cff4edbab666d5
Connor <connordbrown@gmail.com>
2023-11-26 16:44:25 -0700

    add property and setter methods for first_name and last_name attributes in Customer class

--- lib/models/customer.py
+++ lib/models/customer.py
@@ -10,4 +10,29 @@ class Customer:
         self.last_name = last_name
         self.id = id
     
+    def __repr__(self):
+        return f"<Customer #{self.id}: {self.last_name}, {self.first_name}>"
     
+    @property
+    def first_name(self):
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self, first_name):
+        if not isinstance(first_name, str):
+            raise Exception
+        if not len(first_name):
+            raise Exception
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self, last_name):
+        if not isinstance(last_name, str):
+            raise Exception
+        if not len(last_name):
+            raise Exception
+        self._last_name = last_name
